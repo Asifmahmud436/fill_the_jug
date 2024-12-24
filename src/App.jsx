305fd8a80a90cdc8e593b0557162696bb4cf4791
Zettex <safaandsafa4@gmail.com>
2024-12-24 17:21:46 +0600

    adding-decreasing complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import { useState } from 'react'
+import { nanoid } from 'nanoid'
 
 import './App.css'
 
 function App() {
+  const [height,setHeight] = useState(0)
 
+  function increaseWater(){
+    if(height!=600){
+      setHeight(prev=>prev+100)
+    }
+  }
+  function decreaseWater(){
+    if(height!=0){
+      setHeight(prev=>prev-100)
+    }
+  }
   return (
     <>
-      <div>
+      <div className='jug'>
+        
+        
+      <div
+        style={{
+          backgroundColor: 'white',
+          width: "200px",
+          height: height+'px',
+        }}
+        className='water'
+      ></div>
         
       </div>
-      
+      <button onClick={increaseWater}>Increase</button>
+      <button onClick={decreaseWater}>Decrease</button>
     </>
   )
 }
